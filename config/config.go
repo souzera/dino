dd9dbb40d6d69df3ab9903ad72df26edda2082a1
matheus <ps.matheusb@gmail.com>
2024-05-19 16:48:36 -0300

    :triangular_flag_on_post: criando logger

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Init() error{
+	return nil
+}
+
+func GetLogger(p string) *Logger{
+	logger := NewLogger(p)
+	return logger
+}
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import ("github.com/souzera/dino/router")
+import (
+	"github.com/souzera/dino/config"
+	"github.com/souzera/dino/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("DINO")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Erro ao inicializar a aplicação: %v", err)
+		return
+	}
 	router.Initialize()
 }
