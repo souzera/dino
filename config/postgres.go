08a9e3055ad9d64eaf8712070177c9799fa4c75c
matheus <ps.matheusb@gmail.com>
2024-05-24 09:53:59 -0300

    :elephant: tratando migrações do postgres

--- config/postgres.go
+++ config/postgres.go
@@ -22,12 +22,88 @@ func InitializePostgres(host string, user string, password string, dbName string
 
 	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
 	
-	err = db.AutoMigrate(&schemas.Usuario{}, &schemas.Gerencia{}, &schemas.Escola{}, &schemas.Diretor{}, &schemas.Educador{}, &schemas.Aluno{}, &schemas.Turma{}, &schemas.Disciplina{}, &schemas.Nota{}, &schemas.Aula{})
-
+	err = MigrateAll(db)
 	if err != nil {
 		logger.Errorf("Erro ao criar as tabelas: %v", err)
 		return nil, err
 	}
 
 	return db, nil
+}
+
+// MIGRANDO TABELAS
+
+func MigrateUsuario(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Usuario{})
+}
+
+func MigrateGerencia(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Gerencia{})
+}
+
+func MigrateEscola(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Escola{})
+}
+
+func MigrateDiretor(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Diretor{})
+}
+
+func MigrateEducador(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Educador{})
+}
+
+func MigrateAluno(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Aluno{})
+}
+
+func MigrateTurma(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Turma{})
+}
+
+func MigrateDisciplina(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Disciplina{})
+}
+
+func MigrateNota(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Nota{})
+}
+
+func MigrateAula(db *gorm.DB) error{
+	return db.AutoMigrate(&schemas.Aula{})
+}
+
+func MigrateAll(db *gorm.DB) error{
+	if err := MigrateUsuario(db); err != nil {
+		return err
+	}
+	if err := MigrateGerencia(db); err != nil {
+		return err
+	}
+	if err := MigrateEscola(db); err != nil {
+		return err
+	}
+	if err := MigrateDiretor(db); err != nil {
+		return err
+	}
+	if err := MigrateEducador(db); err != nil {
+		return err
+	}
+	if err := MigrateAluno(db); err != nil {
+		return err
+	}
+	if err := MigrateTurma(db); err != nil {
+		return err
+	}
+	if err := MigrateDisciplina(db); err != nil {
+		return err
+	}
+	if err := MigrateNota(db); err != nil {
+		return err
+	}
+	if err := MigrateAula(db); err != nil {
+		return err
+	}
+	
+	return nil
 }
